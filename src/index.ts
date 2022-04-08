8b4de115534df8bf53169e4890f46da02956c5a6
JamesEArnold <jearnold22@gmail.com>
2022-04-07 21:29:36 -0400

    :rotating_light: TBD-001: bandaid fix discordjs type issue and introduce config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import { Client } from 'discord.js';
+import config from '@/config';
 import { logger } from '@/utils/logger';
 import logs from '@/lang/logs';
 
 const start = async () => {
   logger.info('👀 Starting up...');
+
+  const client = new Client({ intents: [ 8 ] });
+
+  client.once('ready', () => {
+    logger.debug('Ready!');
+  });
+
+  client.login(config.client.token);
 };
 
 start().catch((error) => {
--- src/interfaces/IBot.ts
+++
@@ -1,4 +0,0 @@
-export interface IBot {
-  sendMessage(message: string): Promise<void>, 
-  start(): Promise<void>,
-}
