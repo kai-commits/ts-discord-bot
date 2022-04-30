25aa004ace6a6de4ba823bf2e9115a337ec59883
kai-commits <kaimeikle123@gmail.com>
2022-04-30 13:25:41 -0700

    :wrench: TDB-001: add discord intents to config and default it

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ import * as env from 'env-var';
 export default {
   client: {
     token: env.get('BOT_TOKEN').required().asString(),
+    intents: env.get('CLIENT_INTENTS').default(131071).asInt(),
   },
   logger: {
     level: env.get('LOG_LEVEL').default('debug').asString(),
