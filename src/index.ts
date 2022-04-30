db1bd7ba27ac4f04308c611a18e6642f403c544b
kai-commits <kaimeikle123@gmail.com>
2022-04-30 13:30:01 -0700

    :poop: TDB-001: initial message event test code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { Client } from 'discord.js';
 import config from '@/config';
+import { discordMessageHandler } from '@/events';
 import { logger } from '@/utils/logger';
 import logs from '@/lang/logs';
 
 const start = async () => {
-  logger.info('👀 Starting up...');
+  logger.info(logs.operational.startUp);
 
-  const client = new Client({ intents: [ 8 ] });
+  const client = new Client({ intents: config.client.intents });
 
-  client.once('ready', () => {
-    logger.debug('Ready!');
+  client.once('ready', async () => {
+    logger.debug(logs.operational.onReady);
+    client.on('messageCreate',(event) => discordMessageHandler.process(event));
+  });
+
+  client.on('messageCreate', async (event) => {
+    logger.debug('Message Author: ', event.author);
   });
 
   client.login(config.client.token);
